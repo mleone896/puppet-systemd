236b43a93ccd3b17ea9f14e9ad97c9eba8a77b44
Mike Leone <mleone896@gmail.com>
2015-10-20 00:54:44 +0000

    remove the params variables that figure out system path for systemd these files should be dropped into /etc/systemd/system

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,5 +1,4 @@
 class systemd (
-  $unit_path       = $systemd::params::unit_path,
   $service_exports = $systemd::params::service_exports
 ) inherits systemd::params  {
 
--- manifests/params.pp
+++ manifests/params.pp
@@ -2,21 +2,5 @@ class systemd::params {
 
   $service_exports = hiera('systemd::service_exports ', false)
 
-  # figure out the path where service targets go
-  case $::osfamily {
-    'Archlinux': {
-      $unit_path = '/usr/lib/systemd/system'
-    }
-    'Debian': {
-      $unit_path = '/lib/systemd/system'
-    }
-    'RedHat': {
-      $unit_path = '/usr/lib/systemd/system'
-    }
-    default: {
-      $unit_path = '/usr/lib/systemd/system'
-    }
-  }
-
 
 }
