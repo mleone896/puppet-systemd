96707db0f4a2d5889470de5dfe68972bc12c305b
Mike Leone <mleone896@gmail.com>
2016-01-31 11:39:31 -0500

    adding more options to the template that systemd allows

--- manifests/service.pp
+++ manifests/service.pp
@@ -1,16 +1,38 @@
 define systemd::service(
-  $template_file             = 'service.target.erb',
-  $environment_file          = false,
-  $environment_file_location = "/etc/sysconfig/${title}",
-  $user                      = false,
-  $pidfile                   = false,
-  $pidfile_location          = "/var/run/${title}.pid",
-  $start_command             = false,
-  $description               = 'generice service description',
-  $service_exports           = $systemd::params::service_exports
+  $template_file               = 'service.target.erb',
+  $environment_file            = false,
+  $environment_file_location   = "/etc/sysconfig/${title}",
+  $user                        = false,
+  $pidfile                     = false,
+  $pidfile_location            = "/var/run/${title}.pid",
+  $service_after               = undef,
+  $service_type                = 'simple',
+  $exec_start_pre              = undef,
+  $exec_start_post             = undef,
+  $exec_after                  = undef,
+  $exec_reload                 = undef,
+  $exec_stop                   = undef,
+  $exec_stop_post              = undef,
+  $timeout_start_sec           = undef,
+  $restart_sec                 = undef,
+  $timeout_stop_sec            = undef,
+  $timout_sec                  = undef,
+  $restart                     = undef,
+  $description                 = 'generice service description',
+  $success_exit_status         = undef,
+  $restart_prevent_exit_status = undef,
+  $service_exports             = undef,
+  $permissions_start_only      = undef,
+  $nonblocking                 = undef,
+  $sockets                     = undef,
+  $start_limit_action          = undef,
+  $start_limit_interval        = undef,
+  $start_limit_burst           = undef,
+  $failure_action              = undef,
+  $wanted_by                   = 'multi-user.target'
 ) {
 
-
+  # default place for user-created systemd service files
   $service_path        = '/etc/systemd/system'
 
 
